8a1d27a6a276f94a3221715e4b84e6e004919116
James Ren <chrenbingyan@gmail.com>
2018-07-10 23:14:32 -0700

    seperated division in order to better setup the layout

--- src/App.js
+++ src/App.js
@@ -5,21 +5,88 @@ import{
   Link
 } from 'react-router-dom';
 import './Assets/css/default.min.css';
-import Header from './components/headerComponents/header'
-import Footer from './components/footerComponents/footer'
+import Header from './components/pages/header'
+import Footer from './components/pages/footer'
 import HomePage from './components/pages/homePage'
 import Item from './components/pages/item'
 import fetch from 'node-fetch';
 
+var sqlQuery = "SELECT * FROM Stock";
+var form = new FormData();
+form.append("query", sqlQuery);
+
 class App extends Component {
 
-  state = {users: []}
+  state = {users: [], items :[]};
   componentDidMount() {
-    fetch('http://ec2-18-191-61-134.us-east-2.compute.amazonaws.com:3001/users')
+    console.log("componentDidMount");
+    fetch('http://localhost:3001/users')
       .then(res => res.json())
       .then(users => this.setState({ users }));
+
+      // fetch data from mysql database
+      console.log("query is");
+      fetch('http://localhost:3306/queryLocal', {
+          "async": true,
+          "crossDomain": true,
+          "method": "POST",
+          "headers": {
+            "query": "SELECT * FROM Stock",
+            "Cache-Control": "no-cache",
+            "Postman-Token": "d1d3836f-c361-4756-8e64-20bb5428e35f"
+          },
+          "processData": false,
+          "contentType": false,
+          "mimeType": "multipart/form-data",
+          "data": form
+      })
+      .then(res => function (){
+        res.json();
+        console.log("the res is " + res[0]['object_id']);
+        for (var i = 0 ; i < 2 ; i++) {
+          console.log("first item is" + res[0]);
+          console.log(res[0]['object_id']);
+          var a = new Item(res[i]['object_id'],res[i]['picture_id'],res[i]['time_in'],res[i]['time_out'],res[i]['number_of_stock']);
+          console.log("the current a is " + a);
+        }}
+      )
+      .then(items => this.setState({ items }))
   }
 
+  // // fetch data from mysql database
+  // fetchItemData() {
+  //   console.log("query is");
+  //   fetch('http://localhost:3306/queryLocal', {
+  //       "async": true,
+  //       "crossDomain": true,
+  //       "method": "POST",
+  //       "headers": {
+  //         "query": "SELECT * FROM Stock",
+  //         "Cache-Control": "no-cache",
+  //         "Postman-Token": "d1d3836f-c361-4756-8e64-20bb5428e35f"
+  //       },
+  //       "processData": false,
+  //       "contentType": false,
+  //       "mimeType": "multipart/form-data",
+  //       "data": form
+  //   })
+  //   .then(res => res.json())
+  //   .then(rawItems => this.setRawItems({ rawItems }))
+  // }
+
+  //construct an item for each item, and add them to the list
+  constructItems (items) {
+    console.log("construct item");
+    items.map(
+      item => {
+        var a = new Item(item.object_id,item.picture_id,item.time_in,item.time_out,item.number_of_stockd);
+        console.log("the current a is " + a);
+      })
+      // .then(console.log(item))
+      // .then(items.add(item))
+  }
+
+  // create a table of Items
   createTable = () => {
     let table = []
     for (let i = 0; i < 2; i++) {
@@ -51,11 +118,11 @@ class App extends Component {
 
         <Route exact path='/footer' component={Footer}/>
 
-        <Header/>
+        // <Header/>
 
         <Link to ="/homepage"> Homelink </Link>
 
-        <Footer/>
+        // <Footer/>
 
       </div>
       </Router>
--- src/index.js
+++ src/index.js
@@ -2,6 +2,57 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import Header from './components/pages/header';
+import Footer from './components/pages/footer';
+import Catalog from './components/pages/catalog';
+import Item from './components/pages/item';
+import fetch from 'node-fetch';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Header/>, document.getElementById('header'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+for (var i = 1 ; i < 2; i++){
+  var currentItem = document.getElementById('item'+i)
+  currentItem.setAttribute('number',i);
+  function construct() {
+    var sqlQuery = "SELECT * FROM Stock WHERE object_id = " + i;
+    var form = new FormData();
+    form.append("query", sqlQuery);
+    fetch('http://localhost:3306/queryLocal', {
+        "async": true,
+        "crossDomain": true,
+        "method": "POST",
+        "headers": {
+          "query": sqlQuery,
+          "Cache-Control": "no-cache",
+          "Postman-Token": "d1d3836f-c361-4756-8e64-20bb5428e35f",
+        },
+        "processData": false,
+        "contentType": false,
+        "mimeType": "multipart/form-data",
+        "data": form
+    })
+    .then(res => res.json())
+    .then(res => res.item)
+    .then(res => res[0])
+    // return <Catalog/>;
+  }
+  ReactDOM.render(construct(), currentItem);
+}
+ReactDOM.render(<Footer/>, document.getElementById('footer'));
+ReactDOM.render(<Catalog/>, document.getElementById('catalog'));
 registerServiceWorker();
+
+
+
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  ReactDOM.render(element, document.getElementById('second'));
+}
+
+setInterval(tick, 1000);
